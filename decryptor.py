1c545da786d7a1a224cfd0e20a2b6775fc095f05
LeoSuave25 <suaverdez90@gmail.com>
2023-04-15 16:23:16 +0800

    Pseudocode

---
+++ decryptor.py
@@ -0,0 +1,10 @@
+# Style the entire project
+# User Types the Encrypted Text to Decrypt
+# Decrypt the input text by replacing each encrypted character with its original vowel
+# Replace "*" with "a"
+# Replace "&" with "e"
+# Replace "#" with "i"
+# Replace "+" with "o"
+# Replace "!" with "u"
+# Print the decrypted text
+# Put it in a loop so the user can try to Decrypt again

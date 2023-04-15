52c8bc57f94d6ad75e931ad9cf3c413f8e6a9779
LeoSuave25 <suaverdez90@gmail.com>
2023-04-15 16:36:08 +0800

    User Input and Text Replacement is Done

--- decryptor.py
+++ decryptor.py
@@ -1,10 +1,16 @@
 # Style the entire project
 # User Types the Encrypted Text to Decrypt
+encrypted_text = str(input("Enter the encrypted text: "))
 # Decrypt the input text by replacing each encrypted character with its original vowel
 # Replace "*" with "a"
+decrypted_text = encrypted_text.replace("*","a")
 # Replace "&" with "e"
+decrypted_text = decrypted_text.replace("&","e")
 # Replace "#" with "i"
+decrypted_text = decrypted_text.replace("&","e")
 # Replace "+" with "o"
+decrypted_text = decrypted_text.replace("&","e")
 # Replace "!" with "u"
+decrypted_text = decrypted_text.replace("&","e")
 # Print the decrypted text
 # Put it in a loop so the user can try to Decrypt again
